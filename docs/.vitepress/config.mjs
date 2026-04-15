603b1213e64b27eb307e79a2a98e88c484c6ae6e
manishSingh077 <manish.singh@dcmail.ca>
2026-04-14 22:03:51 -0400

    fix base path

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,8 @@ export default defineConfig({
   title: "Manish Singh Portfolio",
   description: "This is going to be a portfolio website for Manish",
 
+  base: '/mtm6407-static-site-manishSingh0777/',
+
   head: [
     [
       'link',
@@ -11,23 +13,6 @@ export default defineConfig({
         rel: 'stylesheet',
         href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
       }
-    ],
-    [
-      'style',
-      {},
-      `
-     
-      .VPNavBarMenuLink {
-        font-size: 18px !important;
-        font-weight: 600;
-      }
-
-     
-      .VPNavBarTitle {
-        font-size: 22px !important;
-        font-weight: 700;
-      }
-      `
     ]
   ],
 
@@ -37,11 +22,7 @@ export default defineConfig({
       { text: 'About', link: '/about' },
       { text: 'Projects', link: '/projects' },
       { text: 'Resume', link: '/resume' },
-      { text: 'Contact', link: '/contact' },
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/manishSingh0777' }
+      { text: 'Contact', link: '/contact' }
     ]
   }
 })
